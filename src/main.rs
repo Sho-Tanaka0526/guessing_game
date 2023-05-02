298ae42664bb76a31373ed92609779bd36493529
Su_Pu#1A4472 <shown22327989@gmail.com>
2023-05-02 15:14:40 +0900

    comment add

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io;                                //入出力のioライブラリの取り込み
 
 fn main() {
-    println!("Guess the number!");          //数を当ててごらん
+    println!("Guess the number!");
 
-    println!("Please input your guess.");   //ほら、予想を入力してね
+    println!("Please input your guess.");
 
     let mut guess = String::new();          //ユーザの入力を格納する変数
 
@@ -11,6 +11,6 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");     //行の読み込みに失敗しました
 
-    println!("You gessed: {}", guess);  //次のように予想しました: {}
+    println!("You gessed: {}", guess);      //次のように予想しました: {}
     
 }
